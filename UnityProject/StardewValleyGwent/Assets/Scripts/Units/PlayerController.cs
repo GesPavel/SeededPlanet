cff1918688f80a85347d9383422650634c1fd144
tenthbyte <51379676+tenthbyte@users.noreply.github.com>
2019-07-09 20:22:05 +0600

    Добавлен префаб игрока. Игрок перемещается по сетке на 1 unit.

---
+++ UnityProject/StardewValleyGwent/Assets/Scripts/Units/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public KeyCode left, right, up, down, use, takeAndPut;
+    public float speed;
+    public float moveDelay;
+    private Rigidbody2D rb2d;
+    private Vector3 lookDirection;
+    private Vector3 moveDirection;
+    private bool moving;
+   
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        moving = false;
+    }
+
+
+    void Update()
+    {
+        MoveControll();
+    }
+
+    private void MoveControll()
+    {
+        if (!moving)
+        {
+            if (Input.GetKey(left))
+            {
+                lookDirection = Vector3.left;
+                moveDirection = lookDirection;
+            }
+            else if (Input.GetKey(right))
+            {
+                lookDirection = Vector3.right;
+                moveDirection = lookDirection;
+            }
+            else if (Input.GetKey(up))
+            {
+                lookDirection = Vector3.up;
+                moveDirection = lookDirection;
+            }
+            else if (Input.GetKey(down))
+            {
+                lookDirection = Vector3.down;
+                moveDirection = lookDirection;
+            }
+            rb2d.transform.up = lookDirection;
+            if (CanMove(moveDirection))
+            {
+                StartCoroutine(movePlayer(moveDirection + transform.position));
+                moveDirection = Vector3.zero;
+            }
+        }
+            
+        
+
+    }
+
+    //Функция будет проверят потенциальное столкновение игрока, если он пойдет по данному направлению.
+    //Если есть препятствие, функция возвращает false.
+    private bool CanMove(Vector3 direction)
+    {
+        return true;
+    }
+
+    //Сопрограмма для перемещения игрока между ячейками.
+    IEnumerator movePlayer(Vector3 destination)
+    {
+        moving = true;
+        float step = speed * Time.deltaTime;
+        float Remainingdistance = (transform.position - destination).sqrMagnitude;
+        while (Remainingdistance > float.Epsilon)
+        {       
+            rb2d.MovePosition(Vector3.MoveTowards(rb2d.position, destination, step));
+            Remainingdistance = (rb2d.transform.position - destination).sqrMagnitude;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        yield return new WaitForSeconds(moveDelay);
+        moving = false;
+    }
+}
