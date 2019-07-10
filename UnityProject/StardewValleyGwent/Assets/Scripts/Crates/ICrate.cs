12accce67acf6fa0859623d2fc24f7df3f5a26d0
GesPavel <ges_p@auca.kg>
2019-07-10 17:53:39 +0600

    * Cоздал интерфейс для ящиков * Создал скрипты для ящика инструментов и ящика семян * Создал префаб ящика семян

---
+++ UnityProject/StardewValleyGwent/Assets/Scripts/Crates/ICrate.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICrate 
+{
+    void TakeFrom();
+    void Put(GameObject gameObject);
+}
---
+++ UnityProject/StardewValleyGwent/Assets/Scripts/Crates/SeedCrate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedCrate : MonoBehaviour, ICrate
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    public void TakeFrom()
+    {
+
+    }
+    public void Put(GameObject seed)
+    {
+
+    }
+}
