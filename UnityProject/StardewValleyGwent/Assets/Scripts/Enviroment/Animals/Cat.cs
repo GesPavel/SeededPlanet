3eda8cf04e7e1681478c91e82295d231b1fe81af
AnnCrudelis <ann.crudelis@gmail.com>
2019-07-22 16:30:42 +0600

    Баг фикс животных

--- UnityProject/StardewValleyGwent/Assets/Scripts/Enviroment/Animals/Cat.cs
+++ UnityProject/StardewValleyGwent/Assets/Scripts/Enviroment/Animals/Cat.cs
@@ -4,16 +4,5 @@ using UnityEngine;
 
 public class Cat : Animal
 {
-    
-    public override void OnCollisionEnter2D(Collision2D coll)
-    {
-        if (coll.gameObject.tag == "ChickenGirl" || coll.gameObject.tag == "Chicken")
-        {
-            Destroy(coll.gameObject);
-            speed = Random.Range(0.5f, 2);
-            direction.x = direction.x * (-1) * Mathf.Sin(Random.Range(2 * Mathf.PI / 3, 4 * Mathf.PI / 3));
-            direction.y = direction.y * (-1) * Mathf.Sin(Random.Range(2 * Mathf.PI / 3, 4 * Mathf.PI / 3));
-            timeForStop = Random.Range(-1, -5);
-        }
-    }
+
 }
