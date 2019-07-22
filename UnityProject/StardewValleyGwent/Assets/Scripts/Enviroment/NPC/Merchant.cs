c15bdce525104d4b746b398224b1535690222de8
GesPavel <ges_p@auca.kg>
2019-07-22 16:37:46 +0600

    * Создал заготовку для Merchant * Создал скрипт для хранения цен на продукты

---
+++ UnityProject/StardewValleyGwent/Assets/Scripts/Enviroment/NPC/Merchant.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Merchant : MonoBehaviour
+{
+    //    MoneyController playersMoney;
+    public GameObject itemToSell;
+    [SerializeField] private float margin = 1.2f;
+    private void Start()
+    {
+//        playersMoney = FindObjectOfType<MoneyController>();
+    }
+
+    public GameObject Trade(GameObject item)
+    {
+        try
+        {
+            if (item != null)
+            {
+                int buyingPrice = DeterminePrice(item);
+                return null;
+            }
+            else
+            {
+                int sellingPrice = (int)(DeterminePrice(itemToSell) * margin);
+                return itemToSell;
+            }
+        } catch(Exception e)
+        {
+            Debug.Log(e.Message);
+            return item;
+        }
+    }
+
+    
+
+    private int DeterminePrice(GameObject item)
+    {
+        return PriceList.GetPriceOf(item);
+    }
+}
---
+++ UnityProject/StardewValleyGwent/Assets/Scripts/Enviroment/NPC/PriceList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PriceList : MonoBehaviour
+{
+    public int priceForVeggie = 25;
+    public int priceForEgg = 50;
+    private static Dictionary<ValuebleItems, int> prices;
+
+    private void Start()
+    {
+        FillPriceList();
+      
+    }
+    enum ValuebleItems
+    {
+        Vegetable, 
+        Egg
+    }
+    private void FillPriceList()
+    {
+        prices = new Dictionary<ValuebleItems, int>();
+        prices.Add(ValuebleItems.Vegetable, priceForVeggie);
+        prices.Add(ValuebleItems.Egg, priceForVeggie);
+        
+    }
+    public static int GetPriceOf(GameObject item)
+    {
+        if (item.GetComponent<Vegetable>() != null)
+        {
+            return prices[ValuebleItems.Vegetable];
+        }
+        else if (item.GetComponent<Egg>() != null)
+        {
+            return prices[ValuebleItems.Egg];
+        }
+        else
+        {
+            throw new Exception("You cannot sell this item");
+        } 
+           
+    }
+}
