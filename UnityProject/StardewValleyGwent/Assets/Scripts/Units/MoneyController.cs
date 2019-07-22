4927ac1b32aff7f9b184a0a92183d715ae2f2128
tenthbyte <51379676+tenthbyte@users.noreply.github.com>
2019-07-22 15:35:26 +0600

    Теперь в интерфейсе отображается количество денег на счете игрока. Игроку добавлен компонент MoneyController.

---
+++ UnityProject/StardewValleyGwent/Assets/Scripts/Units/MoneyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyController : MonoBehaviour
+{
+    [SerializeField]private int initialSum;
+    public static int CurrentSum { get; private set; }
+
+    private void Start()
+    {
+        CurrentSum = initialSum;
+    }
+    public void AddMoney(int sum)
+    {
+        if (sum > 0)
+        {
+            CurrentSum += sum;
+        }
+    }
+
+    public bool Subtract(int sum)
+    {
+        if (sum > CurrentSum) return false;
+        CurrentSum -= sum;
+        return true;
+    }
+}
