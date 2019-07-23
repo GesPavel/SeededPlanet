42775a03f8fdfb2a7a16b512aee95552e3072913
GesPavel <ges_p@auca.kg>
2019-07-23 16:05:57 +0600

    * Создал скрипт удобрения * Добавил возможность удобрять землю * Добавил уменьшение количества удобрений, если на земле растет растение * Добавил зависимость между степенью ухоженности земли и скоростью роста растения на ней * Поправил баг с кроватью. Теперь у нее есть коллайдер, на ней можно спать и не застревать в нем :D

---
+++ UnityProject/StardewValleyGwent/Assets/Scripts/Items/Fertilizer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fertilizer : MonoBehaviour, Instrument
+{
+    public float portionPerUse = 25.0f;
+   public void Use(Ground ground)
+    {
+        ground.AddFertilizer(portionPerUse);
+    }
+}
