d309fd1aedacfff4cf45c036fff557df72eb4a2d
AnnCrudelis <ann.crudelis@gmail.com>
2019-07-26 15:15:15 +0600

    Добавление лопаты

---
+++ UnityProject/StardewValleyGwent/Assets/Scripts/Items/Instruments/Shovel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shovel : MonoBehaviour, Instrument, IItem
+{
+    PlayerController player;
+    Ground standingGround;
+    [SerializeField]
+    private string objectsName;
+    public string ObjectsName => objectsName;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void Use(Ground ground)
+    {
+        if (ground != null)
+        {
+            ground.Raw();
+            ground.DeletePlant();
+
+        }
+    }
+
+}
