adb1085a8e9692f6942437d7d6e2efb6952f0c9d
GesPavel <ges_p@auca.kg>
2019-07-27 22:47:11 +0600

    * Создал скрипт робота-собирателя * Добавил ему возможность передвинаться к месту задачи * Создал скрипт наблюдателя над задачами * Добавил ему возможность взаимодействовать с множеством роботов-наблюдателй * Создал скрипт задачи на сбор * Добавил строчку, создающую задачу на сбор при инстанциированиие растения

---
+++ UnityProject/StardewValleyGwent/Assets/Scripts/Units/Robots/HarvestTask.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HarvestTask : MonoBehaviour
+{
+    public Transform Destination { get; set; }
+    bool inProgress = false;
+    RobotTaskController taskController;
+
+    private void Start()
+    {
+        taskController = FindObjectOfType<RobotTaskController>();
+        taskController.AddHarvestTask(this);
+        Destination = gameObject.transform;
+    }
+    public void ClaimTask()
+    {
+        inProgress = true;
+        taskController.RemoveHarvestTask(this);
+    }
+
+    public void DropTask()
+    {
+        inProgress = false;
+        taskController.AddHarvestTask(this);
+    }
+
+    public bool IsInProgress()
+    {
+        return inProgress;
+    }
+
+
+}
+
