24ca7a23a305e97b42f6af99ab8b029def505397
tenthbyte <51379676+tenthbyte@users.noreply.github.com>
2019-07-29 17:05:52 +0600

    Добавлена система инвентаря.

---
+++ UnityProject/StardewValleyGwent/Assets/InventorySlot.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class InventorySlot : MonoBehaviour
+{
+    [SerializeField] private Sprite placeHolder;
+    [SerializeField] private Image itemsIcon;
+    private string itemsName;
+    private ItemsHolder itemsHolder;
+
+    private void Start()
+    {
+        itemsHolder = ItemsHolder.instance;
+    }
+    public bool IsFool { get; private set; } = false;
+    public void Set(GameObject item)
+    {
+        itemsIcon.sprite = item.GetComponent<SpriteRenderer>().sprite;
+        itemsName = item.GetComponent<IItem>().ObjectsName;
+        IsFool = true;
+    }
+
+    public void Remove()
+    {
+        itemsIcon.sprite = placeHolder;
+        itemsName = null;
+        IsFool = false;
+    }
+
+    public void Drop()
+    {
+        var droppedObject = itemsHolder.GetItemByName(itemsName);
+        Remove();
+        if (droppedObject == null)
+        {
+            return;
+        }
+        droppedObject.transform.position=FindObjectOfType<PlayerController>().transform.position;
+    }
+    public void PutOtemInHand()
+    {
+        HandController handController = FindObjectOfType<HandController>();
+        var objectToHand = itemsHolder.GetItemByName(itemsName);
+        Remove();
+        if (handController.Item != null)
+        {
+            Set(handController.Item);
+            Destroy(handController.Item);
+        }
+        handController.PickUpItem(objectToHand);
+
+    }
+}
---
+++ UnityProject/StardewValleyGwent/Assets/ItemsHolder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemsHolder : MonoBehaviour
+{
+    public ItemAssets itemsList;
+
+    public GameObject GetItemByName(string name)
+    {
+        for(int i = 0; i < itemsList.prefabsList.Count; i++)
+        {
+            IItem item = itemsList.prefabsList[i].GetComponent<IItem>();
+            if (item == null)
+            {
+                continue;
+            }
+            if (item.ObjectsName == name)
+            {
+                var objectToGet = Instantiate(itemsList.prefabsList[i]);
+                return objectToGet;
+            }
+        }
+        return null;
+    }
+
+    #region Singleton
+    public static ItemsHolder instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+}
