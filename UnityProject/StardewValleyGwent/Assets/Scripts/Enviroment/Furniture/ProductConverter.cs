aef0e62167c4fd8c0b28638258849a63d8004179
AnnCrudelis <ann.crudelis@gmail.com>
2019-07-30 16:26:24 +0600

    no message

--- UnityProject/StardewValleyGwent/Assets/Scripts/Enviroment/Furniture/ProductConverter.cs
+++ UnityProject/StardewValleyGwent/Assets/Scripts/Enviroment/Furniture/ProductConverter.cs
@@ -5,37 +5,52 @@ using UnityEngine;
 public class ProductConverter : MonoBehaviour, IConverter
 {
     [System.Serializable]
-    public class InitialandFinalProducts
+    public class InitialAndFinalProducts
     {
         public GameObject initialProduct;
         public GameObject finalProduct;
     }
 
-    public List<InitialandFinalProducts> initialAndFinalProducts;
+    public List<InitialAndFinalProducts> initialAndFinalProducts;
 
     private Dictionary<string, GameObject> productPairs;
 
     private void Start()
     {
         productPairs = new Dictionary<string, GameObject>();
-        foreach(InitialandFinalProducts pair in initialAndFinalProducts)
+        foreach (InitialAndFinalProducts pair in initialAndFinalProducts)
         {
             IItem initialProduct = pair.initialProduct.GetComponent<IItem>();
             if (initialProduct == null) continue;
             if (initialProduct.ObjectsName == "") continue;
-            productPairs.Add(initialProduct.ObjectsName,pair.finalProduct);
+            productPairs.Add(initialProduct.ObjectsName, pair.finalProduct);
         }
+    }
+    public string RetrieveObjectsName(GameObject objectToConvert)
+    {
+        if (objectToConvert == null)
+        {
+            return null;
+        }
+        IItem productToConvert = objectToConvert.GetComponent<IItem>();
+        if (productToConvert == null)
+        {
+            return null;
+        }
+        if (!productPairs.ContainsKey(productToConvert.ObjectsName))
+        {
+            return null;
+        }
+        return productToConvert.ObjectsName;
     }
     public GameObject Convert(GameObject objectToConvert)
     {
-        if (objectToConvert == null) return objectToConvert;
-        IItem productToConvert = objectToConvert.GetComponent<IItem>();
-        if (productToConvert == null) return objectToConvert;
-        if(!productPairs.ContainsKey(productToConvert.ObjectsName))
+        string name = RetrieveObjectsName(objectToConvert);
+        if (name == null)
         {
             return objectToConvert;
         }
-        var finalProduct = Instantiate(productPairs[productToConvert.ObjectsName]);
+        var finalProduct = Instantiate(productPairs[name]);
         Destroy(objectToConvert.gameObject);
         return finalProduct;
     }
