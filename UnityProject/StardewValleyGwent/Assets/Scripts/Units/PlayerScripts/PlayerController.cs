5ba24f6ad65256f478bf90afacbc3f7c3f0f2320
tenthbyte <51379676+tenthbyte@users.noreply.github.com>
2019-08-06 16:27:50 +0600

    Теперь движение игрока плавное. Можно повернуть игрока нажав кнопку движения.

--- UnityProject/StardewValleyGwent/Assets/Scripts/Units/PlayerScripts/PlayerController.cs
+++ UnityProject/StardewValleyGwent/Assets/Scripts/Units/PlayerScripts/PlayerController.cs
@@ -19,7 +19,7 @@ public class PlayerController : MonoBehaviour
     private Vector3 lookDirection;
     private Vector3 destinationPoint;
     private bool moving;
-
+    private bool isplayerTurned = false;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
@@ -47,9 +47,19 @@ public class PlayerController : MonoBehaviour
 
     private void MoveControll()
     {
-        if (!moving)
+        lookDirection = GetLookDirection();
+        if (transform.up != lookDirection && lookDirection!=Vector3.zero)
         {
-            lookDirection = GetLookDirection();
+            transform.up = lookDirection;
+            isplayerTurned = true;
+            return;
+        }
+        if (lookDirection == Vector3.zero)
+        {
+            isplayerTurned = false;
+        }
+        if (!moving && !isplayerTurned)
+        {
             if (lookDirection == Vector3.zero)
             {
                 return;
@@ -63,22 +73,36 @@ public class PlayerController : MonoBehaviour
         }
         else if (moving)
         {
-            MakeStepToDestination(ref destinationPoint);
+            float remainingDistance = (transform.position - destinationPoint).magnitude;
+            if (remainingDistance < speed*Time.fixedDeltaTime)
+            {
+                AttemtContinueMovement();
+            }
+            MakeStepToDestination(destinationPoint);
         }
     }
-    private void MakeStepToDestination(ref Vector3 destinationPoint)
+    private void MakeStepToDestination(Vector3 destinationPoint)
     {
-        float remainingDistance = (transform.position - destinationPoint).magnitude;
-        if (remainingDistance < float.Epsilon)
+        Vector3 newPos = Vector3.MoveTowards(transform.position, destinationPoint, speed * Time.fixedDeltaTime);
+        rb2d.MovePosition(newPos);
+    }
+    private void AttemtContinueMovement()
+    {
+        Vector3 lookDirection = GetLookDirection();
+        if (transform.up == lookDirection)
+        {
+            if (CanMove(lookDirection))
+            {
+                destinationPoint += lookDirection;
+            }
+        }
+        else
         {
             transform.position = destinationPoint;
             moving = false;
             return;
         }
-        Vector3 newPos = Vector3.MoveTowards(transform.position, destinationPoint, speed * Time.fixedDeltaTime);
-        rb2d.MovePosition(newPos);
     }
-
     private Vector3 GetLookDirection()
     {
         Vector3 lookDir = Vector3.zero;
@@ -102,8 +126,8 @@ public class PlayerController : MonoBehaviour
     }
     private bool CanMove(Vector3 direction)
     {
-        RaycastHit2D blockingHit = Physics2D.Raycast(rb2d.transform.position, direction, 1, blockingLayer);
-        RaycastHit2D lakeHit = Physics2D.Raycast(rb2d.transform.position, direction, 1, lakeLayer);
+        RaycastHit2D blockingHit = Physics2D.Raycast(rb2d.transform.position, direction, 1.1f, blockingLayer);
+        RaycastHit2D lakeHit = Physics2D.Raycast(rb2d.transform.position, direction, 1.1f, lakeLayer);
         if (lakeHit.collider || blockingHit.collider != null) return false;
         return true;
     }
@@ -121,5 +145,5 @@ public class PlayerController : MonoBehaviour
     public void MoveToBed()
     {
         transform.position = bed.GetWakeUpPoint().transform.position;
-    }   
+    }
 }
